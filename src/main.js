2e232f3621d87fc45489dea021656f50e5715bea
Allen <331643272@qq.com>
2021-02-18 16:53:50 +0800

    完成用户列表功能的开发“

--- src/main.js
+++ src/main.js
@@ -11,6 +11,14 @@ import axios from 'axios'
 Vue.prototype.$http = axios
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// axios请求拦截
+axios.interceptors.request.use(config => {
+  // 为请求头对象添加 Token验证的Authorization字段
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须return config
+  return config
+})
 Vue.config.productionTip = false
 
 new Vue({
